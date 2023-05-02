bb342cd6859e39a2a02dc2bf96377df40f0664e4
Sai Nikhil Varada <varadasainikhil@gmail.com>
2023-05-02 15:48:03 +0530

    Used dictionary comprehension added exception handling

--- main.py
+++ main.py
@@ -7,9 +7,17 @@ data = pd.read_csv("nato_phonetic_alphabet.csv")
 data_dict = {row.letter: row.code for index, row in data.iterrows()}
 print(data_dict)
 
-# User input
-user_input = input("Enter the word you want to convert: ")
-output_list = [data_dict[letter] for letter in user_input.upper() if letter != " "]
+onlyAlphabets = False
 
-# Printing the final output as a list
-print(output_list)
+while not onlyAlphabets:
+    # User input
+    user_input = input("Enter the word you want to convert: ")
+    try:
+        output_list = [data_dict[letter] for letter in user_input.upper() if letter != " "]
+    except KeyError:
+        print("Please do not enter Special Characters or numbers.")
+        onlyAlphabets = False
+    else:
+        # Printing the final output as a list
+        print(output_list)
+        onlyAlphabets = True
